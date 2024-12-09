73b2742a85576cb348993545377f0d91efdcb865
Hussein Ryalat <hussc.alryalat@gmail.com>
2024-12-09 14:58:23 +0300

    Initial Commit

---
+++ QueueingModels/QueueingModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueueingModelsApp.swift
+//  QueueingModels
+//
+//  Created by Hussein ElRyalat on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct QueueingModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QueueingModelsTests/QueueingModelsTests.swift
@@ -0,0 +1,17 @@
+//
+//  QueueingModelsTests.swift
+//  QueueingModelsTests
+//
+//  Created by Hussein ElRyalat on 9/12/24.
+//
+
+import Testing
+@testable import QueueingModels
+
+struct QueueingModelsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
